9e2ea40bbfec290c1be4d0007bf92062baf9ff15
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-12-30 23:13:11 +0900

    Initial Commit

---
+++ Storytime/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  Storytime
+//
+//  Created by Alan Sparrow on 12/30/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Storytime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Storytime
+//
+//  Created by Alan Sparrow on 12/30/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
