8b14cb261fa8e58daebcb9dd10f106fea916012a
Alan Sparrow <trannguyenbaotrung@gmail.com>
2013-12-31 09:59:04 +0900

    Chapter 24: Storyboard - Init - Finished

---
+++ ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  Storytime
+//
+//  Created by Alan Sparrow on 12/31/13.
+//  Copyright (c) 2013 Alan Sparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
